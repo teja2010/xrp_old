18d420ddfe77510fbbd027e751d3f1e3b29352f3
Tanikella Tejaswi <tteja2010@gmail.com>
2019-01-21 01:22:07 +0530

    Add kernel 4.15 deps

---
+++ deps/kernelsrc/linux-4.15/tools/lib/bpf/feature/test-bpf.d
@@ -0,0 +1,34 @@
+/home/teja/LL/XDP/xrp/deps/kernelsrc/linux-4.15/tools/lib/bpf/feature/test-bpf.bin: \
+ test-bpf.c /usr/include/stdc-predef.h \
+ /home/teja/LL/XDP/xrp/deps/kernelsrc/linux-4.15/tools/arch/x86/include/uapi/asm/unistd.h \
+ /usr/include/x86_64-linux-gnu/asm/unistd_64.h \
+ /home/teja/LL/XDP/xrp/deps/kernelsrc/linux-4.15/tools/include/uapi/linux/bpf.h \
+ /home/teja/LL/XDP/xrp/deps/kernelsrc/linux-4.15/tools/include/linux/types.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stdbool.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/7/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/x86_64-linux-gnu/asm/types.h \
+ /usr/include/asm-generic/types.h /usr/include/asm-generic/int-ll64.h \
+ /home/teja/LL/XDP/xrp/deps/kernelsrc/linux-4.15/tools/arch/x86/include/uapi/asm/bitsperlong.h \
+ /home/teja/LL/XDP/xrp/deps/kernelsrc/linux-4.15/tools/include/asm-generic/bitsperlong.h \
+ /home/teja/LL/XDP/xrp/deps/kernelsrc/linux-4.15/tools/include/uapi/asm-generic/bitsperlong.h \
+ /usr/include/x86_64-linux-gnu/asm/posix_types.h \
+ /usr/include/x86_64-linux-gnu/asm/posix_types_64.h \
+ /usr/include/asm-generic/posix_types.h \
+ /home/teja/LL/XDP/xrp/deps/kernelsrc/linux-4.15/tools/include/uapi/linux/bpf_common.h \
+ /usr/include/unistd.h /usr/include/x86_64-linux-gnu/bits/posix_opt.h \
+ /usr/include/x86_64-linux-gnu/bits/environments.h \
+ /usr/include/x86_64-linux-gnu/bits/confname.h \
+ /usr/include/x86_64-linux-gnu/bits/getopt_posix.h \
+ /usr/include/x86_64-linux-gnu/bits/getopt_core.h
